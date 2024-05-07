801d5b7e4501bf4c32d6504a52299152aec81809
MahdiMurshed <123mahdi12345@gmail.com>
2024-05-07 16:00:38 +0600

    framer motion range slider

--- apps/react/src/routes/framer-motion/interactivity/index.lazy.tsx
+++ apps/react/src/routes/framer-motion/interactivity/index.lazy.tsx
@@ -1,13 +1,18 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
-import { motion } from "framer-motion";
+import { motion, useMotionValue } from "framer-motion";
 import { cn } from "../../../utils/utils";
+import { ChangeEvent } from "react";
 export const Route = createLazyFileRoute("/framer-motion/interactivity/")({
   component: () => <InteractiveButton />,
 });
 
 const InteractiveButton = () => {
+  const scale = useMotionValue(1);
+  const handleRangeChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    scale.set(parseFloat(ev.target.value));
+  };
   return (
-    <div className="flex flex-col items-center text-white p-8 justify-center h-screen">
+    <div className="flex flex-col gap-4 items-center text-white p-8 justify-center h-screen">
       <motion.button
         className={cn(
           "h-16 rounded-full px-8 bg-blue-600",
@@ -25,6 +30,24 @@ const InteractiveButton = () => {
       >
         Hover Me
       </motion.button>
+
+      <motion.div
+        className="bg-black px-6 py-4 rounded-md"
+        style={{
+          scale,
+        }}
+      >
+        Scale
+      </motion.div>
+
+      <input
+        type="range"
+        min={0.5}
+        max={5}
+        step={0.01}
+        defaultValue={1}
+        onChange={handleRangeChange}
+      />
     </div>
   );
 };
